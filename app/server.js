686a8c237736d37561149e57caa2c3c4268e5f97
AhsanAzim <ahsan-azim@live.com>
2016-08-06 00:19:34 -0400

    basic wiring done

--- app/server.js
+++ app/server.js
@@ -1,6 +1,15 @@
+import apiRouter from './router';
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import mongoose from 'mongoose';
+
+// DB Setup
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/blog';
+mongoose.connect(mongoURI);
+// set mongoose promises to es6 default
+mongoose.Promise = global.Promise;
+
 
 // initialize
 const app = express();
@@ -11,6 +20,12 @@ app.use(cors());
 // enable json message body for posting data to API
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', apiRouter);
+app.use('/api/posts/:id', apiRouter);
+app.use('/api/posts', apiRouter);
 
 
 // default index route
