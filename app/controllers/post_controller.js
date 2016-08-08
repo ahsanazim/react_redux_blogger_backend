74953b01fd1c89e4c01c8eb85295d7c27400ee77
AhsanAzim <ahsan-azim@live.com>
2016-08-08 13:06:49 -0400

    testing pt.2

--- app/controllers/post_controller.js
+++ app/controllers/post_controller.js
@@ -51,11 +51,13 @@ export const updatePost = (req, res) => {
       if (err) {
         res.send(err);
       }
+      console.log(`UPDATING: ${req.params.id},${req.body.title},${req.body.tags},${req.body.content}`);
       Post.findById(req.params.id, '_id title tags content',
         (err, docs) => {
           if (err) {
             res.send(err);
           }
+          console.log(`FINDING AFTER UPDTD: ${docs._id},${docs.title},${docs.tags},${docs.content}`);
           const updatedPost = { id: docs._id, title: docs.title, tags: docs.tags, content: docs.content };
           res.json(updatedPost);
         });
